f7ee9f33d84b03b5032e9acc2120fb37a748ac8c
Indexerrowaty <watch998@outlook.com>
2023-08-29 21:11:01 +0200

    📝 Change comments in config_example.js

--- config/config_example.js
+++ config/config_example.js
@@ -22,12 +22,12 @@ module.exports = {
 
 	// Random message when called
 	kopieInformatyka: require("./kopieInformatyka.js"), // Path to message list
-	botnameregexp: /jaros[lł]aw karcewicz|jarkarc/gi, // Jarosław Karcewicz is a very sociable bot. He loves to respond when he sees his name in the chat. He needs a little help to know he's being called though. That's what this regrexp is for.
+	botnameregexp: /jaros[lł]aw karcewicz|jarkarc/gi, // Regexp for detecting when people are talking about the bot.
 	
 	// E-mail "procedure"
 	newslettery: require("./newslettery.js"), // Path to newsletter list
-	emailBlacklist: [], // Which e-mail(s) to block from executing a procedure on?
+	emailBlacklist: [], // E-mail(s) to block from executing the procedure on.
 	emailRegexp: /^(?!\.)[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+/, // E-mail verification regexp.
-	procedureRatelimit: 60000, // For how long rate limit on e-mail procedure should be enforced? Default: 60000ms.
+	procedureRatelimit: 60000, // Length of rate limit for the procedure. Default: 60000ms.
 	
 }
