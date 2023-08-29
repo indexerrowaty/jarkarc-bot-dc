aeb94418966bef90f1fa6f370ecaf4527772919c
Indexerrowaty <watch998@outlook.com>
2023-08-29 05:36:37 +0200

    💩 Temporary walkaround

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,9 +3,9 @@ const { botnameregexp } = require("../config/config.js")
 
 module.exports = {
 	name: "messageCreate",
-	execute(message) {
+	execute(message, bot) {
 		if (message.author.bot) return;
-		if (message.content.match(botnameregexp) || message.mentions.has(bot.user.id)) {
+		if (message.content.match(botnameregexp) || message.mentions.has("1095653965221343232")) {
 			message.reply(kopieInformatyka[Math.floor(Math.random() * kopieInformatyka.length)])
 		}
 	},
