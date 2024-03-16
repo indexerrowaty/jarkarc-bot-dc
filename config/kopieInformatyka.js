5f9f1ae2703101f35a7b94c5b0264aadd97f9a14
indexerrowaty <39349444+indexerrowaty@users.noreply.github.com>
2024-03-16 22:06:40 +0100

    Update kopieInformatyka.js

--- config/kopieInformatyka.js
+++ config/kopieInformatyka.js
@@ -135,5 +135,5 @@ module.exports = [
 	"Po ostatnich doświadczeniach z Waszą usługą stwierdzam, że jesteś najmniej bezpiecznym i godnym polecenia operatorem płatniczym. Przy pierwszych problemach umywacie ręce, nie posiadacie zabezpieczeń dla Waszych użytkowników przed nierzetelnymi kontrahentami. Dlatego wszystkim, którzy chcą mieć poczucie bezpieczeństwa i pewność, że gdy coś się stanie dostaną zwrot pieniędzy ❗️NIE POLECAM❗️ korzystania z usług blik, tylko płatności kartami płatniczymi / kredytowymi, bo ich operatorzy zabezpieczają swoich klientów dużo lepiej niż blik, który wypina się na użytkowników.",
 	"Proszę zgiąć i oderwać wzdłuż perforacji.",
 	"Najpierw proszę usunąć boczne paski. Następnie zgiąć i oderwać wzdłuż perforacji.",
-	"Instrukcja otwarcia na odwrocie",
+	"Instrukcja otwarcia na odwrocie"
 ]
