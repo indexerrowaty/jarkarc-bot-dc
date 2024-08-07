ad0172c51ae4c40b21cd0a5967adf712d8d5420d
Indexerrowaty <watch998@outlook.com>
2024-08-07 03:31:56 +0200

    ✨ AIAIAIAIAI

---
+++ commands/madrosc.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js')
+const { kopieInformatyka } = require("../config/config.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('madrosc')
+		.setDescription('Prezentuje mądrości życiowe Mojej Magnificencji dr. inż. Jarosława!'),
+	async execute(interaction) {
+		await interaction.reply(kopieInformatyka[Math.floor(Math.random() * kopieInformatyka.length)])
+	},
+}
