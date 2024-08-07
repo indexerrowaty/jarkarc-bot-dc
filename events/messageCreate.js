b533c212875cb609285a249db7e64015e10ac6ed
Indexerrowaty <watch998@outlook.com>
2024-08-07 03:31:56 +0200

    ✨ AIAIAIAIAI

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,18 @@
-const { botnameregexp, kopieInformatyka, appID } = require("../config/config.js")
+const { botnameregexp, kopieInformatyka, appID, hfToken, AIprompt } = require("../config/config.js")
+const { HfInference } = require("@huggingface/inference")
+const inference = new HfInference(hfToken);
 
 module.exports = {
 	name: "messageCreate",
-	execute(message) {
+	async execute(message) {
 		if (message.author.bot) return;
 		if (message.content.match(botnameregexp) || message.mentions.has(appID)) {
-			message.reply(kopieInformatyka[Math.floor(Math.random() * kopieInformatyka.length)])
+			const out = await inference.chatCompletion({
+				model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+				messages: [{ role: "user", content: `${AIprompt.replaceAll("${AIusrname}", message.author.displayName)} ${message.content}`}],
+				max_tokens: 2000,
+			}) 
+			message.channel.send(out.choices[0].message.content || kopieInformatyka[Math.floor(Math.random() * kopieInformatyka.length)]);
 		}
 	},
 };
