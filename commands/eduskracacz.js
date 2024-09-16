a1ac5338b4ad85cf5773eb73464f5c861a21a192
index <39349444+indexerrowaty@users.noreply.github.com>
2024-09-16 21:54:46 +0200

    ✨ Add a new innovative command

---
+++ commands/eduskracacz.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('eduskracacz')
+		.setDescription('Pozwala zadrwić z firmy VULCAN, używając ich domeny eduvulcan.pl jako URL shortenera.')
+    .addStringOption(option =>
+			option
+				.setName('URL')
+				.setDescription('Gdzie ma przekierowywać VULCAN?')
+				.setRequired(true)
+    ),
+	execute(interaction) {
+    const plainText = interaction.options.getString("URL")
+    let encodedText = encodeBase32(plainText);
+    encodedText = encodedText.replace(/=+$/, '');
+    interaction.reply(`https://eduvulcan.pl/dziennik/${encodedText}`)
+	},
+}
