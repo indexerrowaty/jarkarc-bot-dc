9dd87a7bd4681cfe2f9ace3ef2622b45f3ddef6b
index <39349444+indexerrowaty@users.noreply.github.com>
2024-09-20 19:08:55 +0200

    🔥 VULCAN killed eduskracacz. What a shame!

--- commands/eduskracacz.js
+++ commands/eduskracacz.js
@@ -1,48 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js')
 
-// Code shamelessly copied from https://es.cvp.ovh/
-const base32Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=";
-
-function encodeBase32(input) {
-    let output = "";
-    let buffer = 0;
-    let bitsLeft = 0;
-
-    for (let i = 0; i < input.length; i++) {
-        buffer = (buffer << 8) | input.charCodeAt(i);
-        bitsLeft += 8;
-
-        while (bitsLeft >= 5) {
-            output += base32Chars[(buffer >> (bitsLeft - 5)) & 31];
-            bitsLeft -= 5;
-        }
-    }
-
-    if (bitsLeft > 0) {
-        output += base32Chars[(buffer << (5 - bitsLeft)) & 31];
-    }
-
-    while (output.length % 8 !== 0) {
-        output += "=";
-    }
-
-    return output;
-}
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('eduskracacz')
-		.setDescription('Pozwala zadrwić z firmy VULCAN, używając ich domeny eduvulcan.pl jako URL shortenera.')
-    .addStringOption(option =>
-			option
-				.setName('url')
-				.setDescription('Gdzie ma przekierowywać VULCAN?')
-				.setRequired(true)
-    ),
+		.setDescription('Pozwalało zadrwić z firmy VULCAN, używając ich domeny eduvulcan.pl jako URL shortenera.'),
 	execute(interaction) {
-    const plainText = interaction.options.getString("url")
-    let encodedText = encodeBase32(plainText);
-    encodedText = encodedText.replace(/=+$/, '');
-    interaction.reply(`https://eduvulcan.pl/dziennik/${encodedText}`)
+    		interaction.reply(`Drodzy użytkownicy,\nzapewne wielu z Was już od początku istnienia tej komendy domyśliło się, jak skończy się ta historia.\nFirma VULCAN zablokowała możliwość przekierowania wszystkiego przez swój URL, a to oznacza, że użyta przez Ciebie komenda już nie zadziała :(\nKomenda zostanie usunięta wkrótce.`, {ephemeral: true})
 	},
 }
