7e3d2660d4621dd6074bf2c20438b923404909dd
Indexerrowaty <watch998@outlook.com>
2025-01-14 23:51:04 +0100

    🚚 Rename example config and create example kopie informatyka

---
+++ config/kopieInformatyka.example.js
@@ -0,0 +1,11 @@
+module.exports = [
+    "Is this launcher safe?",
+    "-----BEGIN PGP SIGNED MESSAGE-----",
+	"-----BEGIN PGP SIGNATURE-----",
+	"-----END PGP SIGNATURE-----",
+	"-----BEGIN PGP PUBLIC KEY BLOCK-----",
+	"-----END PGP PUBLIC KEY BLOCK-----",
+    "\\n",
+    "admin:admin"
+    // You should add more random sentences here to make /madrosc command better.
+]
