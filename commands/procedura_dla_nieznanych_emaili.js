1a47d01debf919ef7e65f011e248ff5237488d20
Indexerrowaty <watch998@outlook.com>
2025-01-18 16:24:03 +0100

    🐛 Minor bug fixes

--- commands/procedura_dla_nieznanych_emaili.js
+++ commands/procedura_dla_nieznanych_emaili.js
@@ -27,11 +27,11 @@ module.exports = {
 				.setDescription('Jak Twój procedurant ma na imię? UWAGA: Jeżeli zostawisz puste, to użyję Twojego nicku!')),
 	async execute(interaction) {
 
-		const usrname = interaction.options.getString("imie") || interaction.user.username || "zjebalosie";
-		let email = interaction.options.getString("email");
+		const usrname = interaction.options.getString("imie") || interaction.user.username || "zjebalosie"
+		let email = interaction.options.getString("email")
 	
 		if (emailBlacklist.includes(email.replaceAll(".", "").toLowerCase().replaceAll("@googlemailcom", "@gmailcom")))
-			return interaction.reply({ content: genericError, ephemeral: true });
+			return interaction.reply({ content: genericError, ephemeral: true })
 	
 		if (!emailRegexp.test(email))
 			return interaction.reply({ content: "## Procedura wykonana niepoprawnie!\nNiepoprawny adres e-mail. Słaby informatyk.", ephemeral: true })
@@ -39,30 +39,22 @@ module.exports = {
 		if (ratelimited > Date.now())
 			return interaction.reply({ content: `## Poczekaj chwilę!\nKomenda będzie dostępna za **${Math.ceil((ratelimited - Date.now())/1000)} sekund**.`, ephemeral: true })
 		
-		ratelimited = Date.now() + procedureRatelimit;
+		ratelimited = Date.now() + procedureRatelimit
 
-		email = encodeURI(email);
+		email = encodeURI(email)
 	
-		await interaction.deferReply();
+		await interaction.deferReply()
 
-		if(!usrname) interaction.editReply({embeds: [warningMessage]})
+		if(!interaction.options.getString("imie")) interaction.editReply({embeds: [warningMessage]})
 
 		let sukcesy = 0
 	
 		for (const e in newslettery.mailerlite) {
+			interaction.editReply(`## Wykonuję procedurę…\nTeraz e-maila próbuje przyjąć: **${newslettery.mailerlite[e][0]}**\nPóki co **${sukcesy}** z **${liczbaNewsletterow}** serwisów skopiowało adres.`)
 			await fetch(`https://app.mailerlite.com/webforms/submit/${newslettery.mailerlite[e][1]}?fields%5Bname%5D=${usrname}&fields%5Bemail%5D=${email}&ml-submit=1&ajax=1`)
 				.then(res => res.json())
-				.then(result => {
-					if (result.success) sukcesy++
-				})
-				.catch(error => {
-					console.error(`PROCEDURA_ERROR [Newsletter ID: ${e} (${newslettery.mailerlite[e][0]})]:`, error)
-				})
-			interaction.editReply(`
-				## Wykonuję procedurę…\n
-				Teraz e-maila próbuje przyjąć: **${newslettery.mailerlite[e][0]}**\n
-				Póki co **${sukcesy}** z **${liczbaNewsletterow}** serwisów skopiowało adres.
-			`)
+				.then(result => result.success ? sukcesy++ : new Error(result))
+				.catch(error =>console.error(`PROCEDURA_ERROR [Newsletter ID: ${e} (${newslettery.mailerlite[e][0]})]:`, error))
 		}
 		
 		const resultMessage = new EmbedBuilder()
@@ -70,8 +62,8 @@ module.exports = {
 			.setTitle("Raport końcowy wykonanej procedury")
 			.setDescription(`**${sukcesy} na ${liczbaNewsletterow}** serwisów pomyślnie skopiowało adres e-mail!`)
 			.setAuthor({ name: "Gotowe!", iconURL: "https://cdn.discordapp.com/attachments/693604907680530442/1145910449095848078/check-mark-icon-png-7.png" })
-			.setTimestamp();
+			.setTimestamp()
 
-		await interaction.editReply({embeds: [resultMessage]})
+		await interaction.editReply({content: "", embeds: [resultMessage]})
 	},
 }
